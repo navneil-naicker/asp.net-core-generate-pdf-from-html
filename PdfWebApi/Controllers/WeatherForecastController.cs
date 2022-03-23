9925f241421a163b6cff4161991e75b263747b70
Navneil Naicker <navneil.naicker@gmail.com>
2022-03-23 17:10:04 +1200

    Initial Commit

---
+++ PdfWebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PdfSharpCore;
+using PdfSharpCore.Pdf;
+using System;
+using System.IO;
+using TheArtOfDev.HtmlRenderer.PdfSharp;
+
+namespace PdfWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public void Get()
+        {
+            var document = new PdfDocument();
+            var html = "<html><body style='color: black'>Hello World.</body></html>";
+            PdfGenerator.AddPdfPages(document, html, PageSize.A4);
+            string path = @"C:\\Temp";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            using (MemoryStream ms = new MemoryStream())
+            {
+                document.Save(path + "\\Test.pdf");
+            }
+        }
+    }
+}
